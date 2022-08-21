2e0238bf7d8d5d9c6e4bb5dcde355c76847faadb
owouwun <kuznetsovivangio@gmail.com>
2022-08-22 00:19:55 +0300

    2nd realisation

--- main1.py
+++ main1.py
@@ -4,12 +4,20 @@ import itertools
 f1 = open('3.csv')
 f2 = open('17.csv')
 
+"""
 people = set()
-
 for row in itertools.chain(csv.reader(f1), csv.reader(f2)):
     if "@" in row[4]:
         if row[4] not in people:
             people.add(row[4])
         else:
             print(row[0], row[1], row[6], row[7], row[9])
-
+"""
+successful = set()
+for row in itertools.chain(csv.reader(f1), csv.reader(f2)):
+    if "@" in row[4]:
+        if row[9] != "-" and int(row[9][:-3]) >= 6:
+            successful.add(row[4])
+        else:
+            if row[4] not in successful:
+                print(row[9], row[6], row[7])
